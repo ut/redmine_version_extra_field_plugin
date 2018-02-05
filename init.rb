f4f3f512d2bbd008ad3ddb0bfa68224a22f4fb19
ut <ulf.treger@googlemail.com>
2018-02-05 14:49:53 +0000

    adding title, description, locales

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_version_extra_field_plugin do
-  name 'Redmine Version Extra Field Plugin plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Redmine Version Extra Field Plugin'
+  author 'Ulf Treger <ulf.treger@googlemail.com>'
+  description 'This is a plugin for Redmine, adding an extra field to the version settings'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/ut/redmine_version_extra_field_plugin'
+  author_url 'https://github.com/ut/'
 end
