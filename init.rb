6876e80285c5ba3e6821e015a123eca782be01c6
ut <ulf.treger@googlemail.com>
2018-02-05 12:54:45 +0000

    initial commit, via redmine plugin generator

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_version_extra_field_plugin do
+  name 'Redmine Version Extra Field Plugin plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
